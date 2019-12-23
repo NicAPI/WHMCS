0d4760d6576f6881321652c830b7eb1ced7cbbb6
JWaldecker <info@lumaserv.com>
2019-12-23 22:24:18 +0100

    Update hooks.php

--- modules/registrars/nicapi/hooks.php
+++ modules/registrars/nicapi/hooks.php
@@ -1,4 +1,7 @@
 <?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
 add_hook('ClientAreaPageDomainDNSManagement', 1, "set_dns_entries");
 
 function set_dns_entries($vars) {
@@ -14,3 +17,24 @@ function set_dns_entries($vars) {
 	];
 	return $supported_dns_records;
 }
+
+
+function nicapi_DomainEdit($variables)
+{
+    include_once '../../modules/registrars/nicapi/nicapi.php';
+    $params = getregistrarconfigoptions("nicapi");
+    if (empty($params))
+        return;
+    $domainID = $variables['domainid'];
+    $domain = (array)Capsule::table('tbldomains')->where('id', '=', $domainID)->first();
+    if ($domain['registrar'] != 'nicapi') return;
+    // user defined configuration values
+    $token = $params['APIKey'];
+    $api = new NicAPIClient($token);
+    if ($domain['donotrenew']) {
+        nicapi_RequestDeleteExpire($params+$domain);
+    } else {
+        nicapi_CancelExpireDelete($params+$domain);
+    }
+}
+add_hook("DomainEdit", 1, "nicapi_DomainEdit");
